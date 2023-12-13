eb11f251f1ab7df72c298c4aabe5e94bae9546a2
euginee23 <eugineimba123@gmail.com>
2023-12-13 15:17:16 +0800

    second commit

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'localhost', 
-  user: 'root',      
-  password: '',      
-  database: 'ads_booking_db',
+  host: 'bezk5g5ddjyvpjflbhib-mysql.services.clever-cloud.com', 
+  user: 'ujy53jqbnb8ngjkw',      
+  password: '7wdycmcCNN3gY1TkMq6h',      
+  database: 'bezk5g5ddjyvpjflbhib',
 });
 
 module.exports = pool
